219bea946ffcbfe504665426dc05b349c3580ab9
Geoff Wilson <gw@yay.com>
2016-07-09 11:21:05 +0100

    Add Database support.

--- whfc/create_app.py
+++ whfc/create_app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from oslo_config import cfg
 
 app = Flask(__name__)
 login_manager = LoginManager()
+db = SQLAlchemy()  # Flask-SQLAlchemy extension
 
 flask_opts = [
     cfg.BoolOpt('debug',
@@ -17,11 +19,18 @@ flask_opts = [
                help='secret key for flask'),
 ]
 
+# These are the configurations settings for SQLAlchemy
+database_opts = [
+    cfg.StrOpt('connection_string'),
+    cfg.StrOpt('track_modifications')
+]
+
 
 class FlaskConfig(object):
     def __init__(self):
         conf = cfg.CONF
         conf.register_opts(flask_opts, 'flask')
+        conf.register_opts(database_opts, 'database')
 
         for key, value in conf.flask.items():
             setattr(self, key.upper(), value)
@@ -37,6 +46,10 @@ def create_app(config_files=None):
     # Flask Config File Configuration
     app.config.from_object(FlaskConfig())
 
+    # Flask-SQLAlchemy configuration
+    app.config['SQLALCHEMY_DATABASE_URI'] = conf.database.connection_string
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = conf.database.track_modifications
+
     # Misc Jinja Configuration
     app.jinja_env.lstrip_blocks = True
     app.jinja_env.trim_blocks = True
@@ -44,5 +57,11 @@ def create_app(config_files=None):
     # LoginManager Configuration
     login_manager.init_app(app)
     login_manager._login_disabled = False
+
+    # Set Flask-Login session protection to the highest level
+    login_manager.session_protection = 'strong'
+
+    # Initialize the extensions we are using
+    db.init_app(app)
 
     return app
